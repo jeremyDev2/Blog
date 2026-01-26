d80ad47b1525c34866df7a71e0e9d7902d061468
Daniel <danil.malaev3@gmail.com>
2026-01-26 14:56:41 +0200

    initial commit

---
+++ blog/blog_app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+# The logic of your application goes here; each view receives an HTTP request, pro-
+# cesses it, and returns a response.
+# Create your views here.
+
+
