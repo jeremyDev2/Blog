213d5ed3c2d3cdf5bd0ab2c0acd96fa5ad843087
Daniel <danil.malaev3@gmail.com>
2026-01-27 22:58:47 +0200

    fix typos

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,17 +1,13 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import get_object_or_404, render
+
 from .models import Post
-from django.http import Http404
-# The logic of your application goes here; each view receives an HTTP request, pro-
-# cesses it, and returns a response.
-# Create your views here.
 
 def post_list(request):
-    post = Post.published.all()
-    return render(request,'blog/post/list.html', {'post':post})
+    posts = Post.published.all()
+    return render(request, "blog/post/list.html", {"posts": posts})
 
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id, status = Post.Status.PUBLISHED)
-        
-    return render(request,'blog/post/detail.html', {'post': post})
 
- 
+    return render(request, "blog/post/detail.html", {"post": post})
+
