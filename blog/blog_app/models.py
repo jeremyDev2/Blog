59b50e6dc5bacfa3783ce9a3aa4ca74b576e606c
Daniel <danil.malaev3@gmail.com>
2026-01-29 23:31:33 +0200

    initialize tag object

--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -31,6 +32,8 @@ class Post(models.Model):
     objects  = models.Manager()
     published = PublishedManager()
     
+    tags = TaggableManager()
+
     class Meta:
         ordering = ['publish'] # ordering attribute to tell Django that it should sort results by the publish field
         indexes = [models.Index(fields=['-publish']),]
