fcc52c90019d105faab36420774d099db9ed63cb
Danial <114513984+jeremyDev2@users.noreply.github.com>
2026-03-21 01:50:22 +0200

    Update settings.py to enhance security
    Removed trash

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -27,10 +27,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hf_a-frd)mu$c-6iv+y_96x7y9cjjtn!p@9(*ou)e@)xpyfdvs'
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
